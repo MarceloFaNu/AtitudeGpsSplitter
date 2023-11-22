5387f1f2eacfefbc2031e431b1fbb69be695e0dd
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-11-21 23:35:21 -0300

    Criados scripts de JavaScript para manuseio do LocalStorage.

---
+++ AtitudeGpsSplitterApp/wwwroot/scriptsGlobais.js
@@ -0,0 +1,17 @@
+﻿window.localStorageManager = {
+    adicionaItem: function (chave, valor) {
+        localStorage.setItem(chave, valor);
+    },
+
+    obtemItem: function (chave) {
+        return localStorage.getItem(chave);
+    },
+
+    retiraItem: function (chave) {
+        localStorage.removeItem(chave);
+    },
+
+    limpaStorage: function () {
+        localStorage.clear();
+    }
+}
