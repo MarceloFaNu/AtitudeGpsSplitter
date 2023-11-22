8705c20b9375c77865b2d84b74bb696215be39b3
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-11-21 23:35:58 -0300

    Criadas constantes para servirem como chaves do LocalStorage.

---
+++ AtitudeGpsSplitterApp/Domain/Constantes/ChavesDoLocalStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace AtitudeGpsSplitterApp.Domain.Constantes
+{
+    public class ChavesDoLocalStorage
+    {
+        public const string CHAVE_LOCAL_STORAGE_RESUMO = "chaveResumo";
+        public const string CHAVE_LOCAL_STORAGE_COPILOTO = "chaveCopiloto";
+        public const string CHAVE_LOCAL_STORAGE_MONITOR = "chaveMonitor";
+    }
+}
