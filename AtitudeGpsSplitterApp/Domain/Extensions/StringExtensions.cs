4fa756b47927a177ee6343f498f9eb6f9f912efb
Marcelo Fagundes Nunes <marcelo.fagundes@modalgr.com.br>
2023-11-25 17:02:20 -0300

    Implementada validação do preenchimento das áreas de textos de logs.

---
+++ AtitudeGpsSplitterApp/Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AtitudeGpsSplitterApp.Domain.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool HasJsonFormatString(this string value)
+        {
+            return value.StartsWith("{") && value.EndsWith("}");
+        }
+    }
+}
